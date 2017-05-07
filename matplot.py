fa607d57584d13251dd12e0c5e2d8b3863c66f1e
davidlwl <davidlwl3@gmail.com>
2017-05-07 23:53:14 +0800

    Add files via upload

---
+++ matplot.py
@@ -0,0 +1,16 @@
+import pandas as pd
+import datetime
+from pandas_datareader import data
+start = datetime.datetime(2010, 1, 1)
+end = datetime.datetime(2015, 8, 22)
+df = data.DataReader("XOM", "yahoo", start, end)
+
+print(df.head())
+
+import matplotlib.pyplot as plt
+from matplotlib import style
+
+style.use('fivethirtyeight')
+df['High'].plot()
+plt.legend()
+plt.show()
